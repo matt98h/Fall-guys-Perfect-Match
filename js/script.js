13842781dbb9c7bd77115c7eb98b0b22d9dc4d39
Keigal <oronoco.alex@gmail.com>
2020-09-02 17:58:08 -0500

    cleaned and commited file, added logic to prevent empty images

--- js/script.js
+++ js/script.js
@@ -1,34 +1,41 @@
+// declaring global variables
 var fruitSelected = "";
 var previousFruit = "";
 
+// this function waits for a fruit selection button to be pressed, then saves the selection to fruitSelected
 function selectFruit(fruit){
 
     // sets the global variable of selected fruit
     previousFruit = fruitSelected
     fruitSelected = fruit
 
-    // adds selected-button class to selected button
-    el = document.getElementById(fruit)
-    el.classList.add('selected-button')
-
     // removes selected-button class from previous button
     if (previousFruit != null) {
         el = document.getElementById(previousFruit)
         el.classList.remove('selected-button')
     }
+
+    // adds selected-button class to selected button
+    el = document.getElementById(fruit)
+    el.classList.add('selected-button')
     
 }
 
+// gets current fruitSelected and adds image to clicked box
 function writeFruit(box){
+
+    if(fruitSelected === ""){
+        return;
+    }
 
     el = document.getElementById(box)
     
-    //      <img class="fruit-image" src="./images/Apple.jpeg">
     el.innerHTML = '<img class="fruit-image" src="./images/' + fruitSelected + '.png">'
 
     console.log(el)
 }
 
+// refreshes page
 function reset(){
 
     window.location.reload();
